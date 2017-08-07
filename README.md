62003250d8949ba687dc6a7f9f0f91408ccfacaf
Gregory <greghojob@gmail.com>
2017-08-07 10:10:04 +0800

    Add some demo, and update README

---
+++ README.md
@@ -0,0 +1,4 @@
+# MQTT Manager
+A connection / subscribe / publish handler of MQTT
+
+[![license](https://github.com/GregoryHo/MqttManager/blob/master/LICENSE)](https://github.com/GregoryHo/MqttManager/blob/master/LICENSE)
--- app/src/main/java/com/ns/greg/mqttmanager/DemoActivity.java
+++ app/src/main/java/com/ns/greg/mqttmanager/DemoActivity.java
@@ -4,6 +4,12 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import com.ns.greg.library.mqtt_manager.MqttManager;
+import com.ns.greg.library.mqtt_manager.internal.Connection;
+import com.ns.greg.library.mqtt_manager.internal.MqttActionListener;
+import com.ns.greg.library.mqtt_manager.internal.Publishing;
+import com.ns.greg.library.mqtt_manager.internal.Subscription;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Gregory
@@ -17,6 +23,63 @@ public class DemoActivity extends AppCompatActivity {
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    MqttManager.getInstance();
+    Connection connection = MqttManager.getInstance().getConnection(CLIENT_ID);
+    if (connection == null) {
+      connection = MqttManager.getInstance()
+          .addConnection(
+              Connection.createConnectionWithTimeStamp(getApplicationContext(), "demo.url",
+                  CLIENT_ID));
+    }
+
+    // connection
+    connection.connect(new MqttActionListener.OnActionListener() {
+      @Override public <T extends Subscription> void onSuccess(T subscription, String message) {
+
+      }
+
+      @Override public void onFailure(Subscription subscription, Throwable throwable) {
+
+      }
+    });
+
+    // subscribe single, the manager will check the connection is connected or not
+    // if not, the manager will automatic connect to server and subscribe when connected
+    connection.subscribeTopic(new Subscription("/door"), new MqttActionListener.OnActionListener() {
+      @Override public <T extends Subscription> void onSuccess(T subscription, String message) {
+
+      }
+
+      @Override public void onFailure(Subscription subscription, Throwable throwable) {
+
+      }
+    }, 0);
+
+    // subscribe multiple, the manager will check the connection is connected or not
+    // if not, the manager will automatic connect to server and subscribe when connected
+    List<Subscription> subscriptionList = new ArrayList<>();
+    subscriptionList.add(new Subscription("/window"));
+    subscriptionList.add(new Subscription("/light"));
+    connection.subscribeTopics(subscriptionList, new MqttActionListener.OnActionListener() {
+      @Override public <T extends Subscription> void onSuccess(T subscription, String message) {
+
+      }
+
+      @Override public void onFailure(Subscription subscription, Throwable throwable) {
+
+      }
+    }, 0);
+
+    // publish, the manager will check the connection is connected or not
+    // if not, the manager will automatic connect to server and publish when connected
+    connection.publishTopic(new Publishing("/door", "close"),
+        new MqttActionListener.OnActionListener() {
+          @Override public <T extends Subscription> void onSuccess(T subscription, String message) {
+
+          }
+
+          @Override public void onFailure(Subscription subscription, Throwable throwable) {
+
+          }
+        });
   }
 }
